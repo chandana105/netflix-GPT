04fb99db1f229b53dacdc0186610c13466db0912
chandana105 <39641650+chandana105@users.noreply.github.com>
2023-11-28 11:48:45 +0530

    feat(createreactapp and tailwind setup)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-amber-400">
+        Namastey React Netflix GPT
+      </h1>
+    </>
+  );
+}
+
+export default App;
